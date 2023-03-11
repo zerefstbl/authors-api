b016822dc248502da9fd5971e1d0898a951da9ec
Eduardo de Oliveira <duduoli11@hotmail.com>
2023-03-10 21:22:27 -0300

    News folders, docker and new files

--- core_apps/users/admin.py
+++ core_apps/users/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .forms import UserChangeFormm, UserCreationForm
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    add_form = UserCreationForm
+    form = UserChangeFormm
+    model = User
+    list_display = [
+        'pkid',
+        'id',
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+    ]
+    list_display_links = [
+        'id',
+        'email',
+    ]
+    list_filter = [
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+    ]
+    fieldsets = (
+        (
+            _('Login Credentials'),
+            {
+                'fields': (
+                    'email',
+                    'password',
+                    
+                )
+            },
+        ),
+        (
+            _('Personal Informationm'),
+            {
+                'fields': (
+                    'username',
+                    'first_name',
+                    'last_name',
+                )
+            },
+        ),
+        (
+            _('Permissions and Groups'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                )
+            },
+        ),
+        (
+            _('Important Dates'),
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                )
+            },
+        ),
+    )
---
+++ docker/local/postgres/maintenance/_sourced/messages.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+message_newline(){
+  echo
+}
+
+message_debug() {
+  echo -e "DEBUG: ${@}"
+}
+
+message_welcome() {
+  echo -e "\e[1m${@}\e[0m"
+}
+
+message_warning() {
+  echo -e "\e[33mWARNING\e[0m: ${@}"
+}
+
+message_error() {
+  echo -e "\e[31merror\e[0m: ${@}"
+}
+
+message_info() {
+  echo -e "\e[37mINFO\e[0m: ${@}"
+}
+
+message_suggestion() {
+  echo -e "\e[33mSUGGESTION\e[0m: ${@}"
+}
